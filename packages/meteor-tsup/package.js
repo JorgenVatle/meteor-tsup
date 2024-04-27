c1a912493582da5255c156855284acad7e041d5b
Jørgen Vatle <jorgen@wcaservices.net>
2024-04-27 15:20:16 +0300

    Add build plugin registration

--- packages/meteor-tsup/package.js
+++ packages/meteor-tsup/package.js
@@ -1,9 +1,21 @@
+const dependencies = {
+  ecmascript: 'ecmascript@0.16.8 || 1.0.0',
+}
+
 Package.describe({
   name: 'jorgenvatle:meteor-tsup',
   version: '1.0.0',
   summary: 'Pre-bundle and watch Meteor project files using ESBuild with source map support.',
   git: 'https://github.com/JorgenVatle/meteor-tsup',
   documentation: 'README.md'
+});
+
+Package.registerBuildPlugin({
+  name: 'tsup',
+  use: [
+    dependencies.ecmascript,
+  ],
+  sources: ['plugin.js'],
 });
 
 Package.onUse(function(api) {});
