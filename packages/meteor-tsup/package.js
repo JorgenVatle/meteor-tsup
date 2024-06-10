d1c70cde9b41c205f3392868438739eb622239ba
Jørgen Vatle <jorgen@wcaservices.net>
2024-06-10 22:40:40 +0300

    Release v2 of package exclusively for Meteor v3

--- packages/meteor-tsup/package.js
+++ packages/meteor-tsup/package.js
@@ -1,11 +1,11 @@
 const dependencies = {
-  ecmascript: 'ecmascript@0.16.8 || 0.16.8-beta300.6 || 0.16.8-rc300.0 || 1.0.0',
+  ecmascript: '0.16.8-beta300.6 || 0.16.8-rc300.0 || 0.16.9-rc300.2',
   compilerPlugin: 'isobuild:compiler-plugin@1.0.0',
 }
 
 Package.describe({
   name: 'jorgenvatle:tsup',
-  version: '1.0.0',
+  version: '2.0.0',
   summary: 'Pre-bundle and watch Meteor project files using ESBuild with source map support.',
   git: 'https://github.com/JorgenVatle/meteor-tsup',
   documentation: 'README.md'
